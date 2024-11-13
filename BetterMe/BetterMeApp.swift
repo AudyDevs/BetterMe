c824f3aeaede95e76de608268d1c4ebb8e1dcbf3
AudyDev <audyDev@gmail.com>
2024-11-13 12:54:08 +0100

    Initial Commit

---
+++ BetterMe/BetterMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterMeApp.swift
+//  BetterMe
+//
+//  Created by AudyDev on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct BetterMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
